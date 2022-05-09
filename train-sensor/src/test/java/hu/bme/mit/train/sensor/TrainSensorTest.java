d37f9b264fa8d220ec708aec47923e16f26882af
davidpete9 <40660676+davidpete9@users.noreply.github.com>
2022-05-09 17:08:41 +0200

    Update TrainSensorTest.java

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -22,8 +22,8 @@ public class TrainSensorTest {
 
     @Before
     public void before() {
-        this.ctrl = mock(TrainController.class);
-        this.user = mock(TrainUser.class);
+        this.ctrl = mock(TrainControllerImpl.class);
+        this.user = mock(TrainUserImpl.class);
         this.sensor = new TrainSensorImpl(this.ctrl, this.user);
     }
 
