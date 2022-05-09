2efdb32337283d37be7ca436bf11d1f89a351442
davidpete9 <40660676+davidpete9@users.noreply.github.com>
2022-05-09 16:50:51 +0200

    Update TrainSensorTest.java

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -3,9 +3,7 @@ package hu.bme.mit.train.sensor;
 import org.junit.Assert;
 import org.junit.Before;
 import hu.bme.mit.train.interfaces.*;
-import hu.bme.mit.train.controller.TrainControllerImpl;
-import hu.bme.mit.train.sensor.TrainSensorImpl;
-import hu.bme.mit.train.user.TrainUserImpl;
+import hu.bme.mit.train.*;
 import org.junit.Test;
 import static org.mockito.Mockito.*;
 
