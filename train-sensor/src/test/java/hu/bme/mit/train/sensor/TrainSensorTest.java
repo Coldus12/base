27923aff085a2c7a807c0d8a78f02db974a21fe2
davidpete9 <40660676+davidpete9@users.noreply.github.com>
2022-05-09 16:57:56 +0200

    Update TrainSensorTest.java

--- train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
+++ train-sensor/src/test/java/hu/bme/mit/train/sensor/TrainSensorTest.java
@@ -30,26 +30,26 @@ public class TrainSensorTest {
     @Test
     public void SpeedLimitIsNegativeTest() {
         this.sensor.overrideSpeedLimit(-10);
-        when(this.user.getAlarmState()).thenReturn(true);
+        verify(this.user.getAlarmState()).thenReturn(true);
     }
 
     @Test
     public void SpeedLimitIsTooLargeTest() {
         this.sensor.overrideSpeedLimit(501);
-        when(this.user.getAlarmState()).thenReturn(true);
+        verify(this.user.getAlarmState()).thenReturn(true);
     }
 
     @Test
     public void SpeedLimitRelativeMarginExceededTest() {
         this.ctrl.setReferenceSpeed(100);
         this.sensor.overrideSpeedLimit(40);
-        when(this.user.getAlarmState()).thenReturn(true);
+        verify(this.user.getAlarmState()).thenReturn(true);
     }
 
     @Test
     public void SpeedLimitRelativeMarginNotExceededTest() {
         this.ctrl.setReferenceSpeed(100);
         this.sensor.overrideSpeedLimit(60);
-        when(this.user.getAlarmState()).thenReturn(false);
+        verify(this.user.getAlarmState()).thenReturn(false);
     }
 }
